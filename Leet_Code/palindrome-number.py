de42f71675ea26250002dd5c150bdcc0437c888f
Fraol-M <fraolbacha111@gmail.com>
2024-07-18 11:46:04 +0300

    Create palindrome-number.py

---
+++ Leet_Code/palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        m = str(x)
+        if m == m[::-1]:
+            return True
+        else:
+            return False
+        
