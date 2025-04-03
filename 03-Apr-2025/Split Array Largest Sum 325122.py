68c2fe300d43e4aab3b7312c935a22e1b9ab4d9c
Fraol Megersa <fraolbacha111@gmail.com>
2025-04-03 18:36:07 +0300

    Created 'Split Array Largest Sum' by A2SV Hub

---
+++ 03-Apr-2025/Split Array Largest Sum 325122.py
@@ -0,0 +1,26 @@
+# Problem: Split Array Largest Sum - https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+    
+    
+        def feasible(threshold):
+            count = 1
+            total = 0
+            for num in nums:
+                total += num
+                if total > threshold:
+                    total = num
+                    count += 1
+                    if count > k:
+                        return False
+            return True
+
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid     
+            else:
+                left = mid + 1
+        return left
